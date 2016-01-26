fa128c664f91be23333112e8561ece3b11d16773
Arianna Delin <arianna.delin11@ufl.edu>
2016-01-25 20:30:50 -0500

    updated css and index files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Web Interactivity and Engagement </title>
+	<link rel= "stylesheet" type="text/css" href="css">
+
+</head>
+<body>
+	<h1> Web Interactivity and Engagement </h1>
+	<h2> Spring 2016 </h2>
+
+	<p>This is where the blogs will go. </p>
+
+</body>
+</html>	
+
