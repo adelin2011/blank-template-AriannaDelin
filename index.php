42e44f7381f8961138ce2ee500974aa906cc213a
Arianna Delin <arianna.delin11@ufl.edu>
2016-02-09 19:48:40 -0500

    moved footer content from index to footer.php as shown in class. Moved to template folder.

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title> Web Interactivity and Engagement </title>
-	<link rel= "stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+<?php get_header(); ?>
 
-</head>
-<body>
-	<h1> Web Interactivity and Engagement </h1>
-	<h2> Spring 2016 </h2>
+	<section class="row">
+		<div class="nine columns">
+			<?php 
+				if (have_posts()){
+					while (have_posts()){
+						the_post();?>
+						<h3> <?php the_title (); ?></h3>
+						<?php if ( has_post_thumbnail() ) {
+               			 the_post_thumbnail('thumbnail');
+           		 		} ?>
+						<?php the_excerpt();?>
+						<a href="<?php the_permalink(); ?>" Read More... </a>						
+					<?php }//end while
+				}//end if
+				?>
+		</div>	
+		<div class="three columns"> 
+			<h4>Sidebar </h4>
+			<p>Sidebar </p>
+		</div>
+	</section>	
 
-	<p>This is where the blogs will go. </p>
-
-</body>
-</html>	
-
+<?php get_footer(); ?>
